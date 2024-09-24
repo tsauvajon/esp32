2dc8491a23063abc08f21356e17507b9030c06f2
Thomas Sauvajon <thomas@sauvajon.tech>
2024-09-24 22:46:17 +0200

    Make a LED blink

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
+run:
+	cargo build
+	espflash flash --monitor target/xtensa-esp32-espidf/debug/esp32
+
 deploy:
 	cargo build
 	espflash flash target/xtensa-esp32-espidf/debug/esp32
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,27 @@
-fn main() {
+use std::{thread::sleep, time::Duration};
+
+use esp_idf_svc::{
+    hal::{gpio::PinDriver, prelude::Peripherals},
+    sys::EspError,
+};
+
+fn main() -> Result<(), EspError> {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_svc::sys::link_patches();
 
-    // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
+    let peripherals = Peripherals::take()?;
+    let mut led = PinDriver::output(peripherals.pins.gpio5)?;
+
     log::info!("Hello, world!");
+
+    loop {
+        led.set_high()?;
+        sleep(Duration::from_millis(1000));
+
+        led.set_low()?;
+        sleep(Duration::from_millis(1000));
+    }
 }
