2bb97671ad96d9394b0baf6b8d60d3dbe0c4232d
Thomas Sauvajon <thomas.sauvajon@proton.me>
2025-11-06 14:28:06 +0100

    initial cleanup

--- ultrasound/src/lib.rs
+++ ultrasound/src/lib.rs
@@ -1,1 +1,15 @@
 #![no_std]
+
+use esp_hal::{
+    peripherals::Peripherals,
+    time::{Duration, Instant},
+};
+use log::info;
+
+pub fn run(_peripherals: Peripherals) -> ! {
+    loop {
+        info!("Hello world!");
+        let delay_start = Instant::now();
+        while delay_start.elapsed() < Duration::from_millis(500) {}
+    }
+}
