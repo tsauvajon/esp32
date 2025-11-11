d7be5a17174c6ee5febc3d63a29303b048d8cf31
Thomas Sauvajon <thomas.sauvajon@proton.me>
2025-11-11 17:08:03 +0100

    Fix initiating the radio with a static lifetime

--- webserver/src/bin/main.rs
+++ webserver/src/bin/main.rs
@@ -12,9 +12,8 @@ use embedded_hal_async::delay::DelayNs;
 use esp_backtrace as _;
 use esp_hal::clock::CpuClock;
 use esp_hal::timer::timg::TimerGroup;
+use esp_radio::Controller;
 use esp_radio::wifi::Config as WifiConfig;
-use esp_radio::wifi::Interfaces;
-use esp_radio::wifi::WifiController;
 use log::info;
 use webserver::mk_static;
 use webserver::web::WebApp;
@@ -39,12 +38,10 @@ async fn main(spawner: Spawner) -> ! {
     info!("Embassy initialized!");
 
     let rng = esp_hal::rng::Rng::new();
-    let radio_init = esp_radio::init().unwrap();
+    let radio_init = &*mk_static!(Controller, esp_radio::init().unwrap());
 
-    let (wifi_controller, interfaces) = &*mk_static!(
-        (WifiController<'static>, Interfaces<'static>),
-        esp_radio::wifi::new(&radio_init, peripherals.WIFI, WifiConfig::default()).unwrap()
-    );
+    let (wifi_controller, interfaces) =
+        esp_radio::wifi::new(&radio_init, peripherals.WIFI, WifiConfig::default()).unwrap();
 
     let stack = start_wifi(wifi_controller, interfaces, rng, &spawner).await;
     let web_app = WebApp::default();
