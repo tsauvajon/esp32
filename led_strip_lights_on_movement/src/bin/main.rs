10e95a1e9bf37a9afd82c9b682add5fa9a9786e8
Thomas Sauvajon <thomas.sauvajon@proton.me>
2025-11-19 20:32:47 +0100

    Extract a trait from the MotionDetector, so it can be tested out without needing an actual PIR motion sensor plugged in + long compile/upload times

--- led_strip_lights_on_movement/src/bin/main.rs
+++ led_strip_lights_on_movement/src/bin/main.rs
@@ -39,6 +39,7 @@ fn main() -> ! {
         movement_detection_pin,
         InputConfig::default().with_pull(Pull::Down),
     );
+    let motion_sensor = PirMotionSensor(sensor_pin);
 
     let delay = Delay::new();
     let led_control = build_led_controller(peripherals.RMT, led_strip_data_pin);
@@ -48,23 +49,23 @@ fn main() -> ! {
     info!("Delaying start - {STARTUP_DELAY}!");
     delay.delay(STARTUP_DELAY);
 
-    driver.light_off();
+    driver.light_off().unwrap();
 
     let mut triggers = 0;
     loop {
-        if sensor_pin.is_high() {
+        if motion_sensor.motion_detected() {
             triggers += 1;
             // Avoid false positives, by requiring detection several times in a row
             if triggers >= REQUIRED_CONSECUTIVE_DETECTIONS {
                 info!("Initial motion!");
-                driver.keep_on_until_silence(&sensor_pin).unwrap();
+                driver.keep_on_until_silence(&motion_sensor).unwrap();
                 triggers = 0;
                 continue;
             }
 
             info!("Trigger {triggers}");
         } else {
-            driver.light_off();
+            driver.light_off().unwrap();
             triggers = 0;
         }
 
@@ -84,11 +85,16 @@ impl<'a> Driver<'a> {
         self.led_control.tick(elapsed_in_ms)
     }
 
-    fn light_off(&mut self) {
+    fn light_off(&mut self) -> Result<(), ClocklessRmtError> {
         self.led_control.set_brightness(0.0);
+        let elapsed_in_ms = blinksy_esp::time::elapsed().as_millis();
+        self.led_control.tick(elapsed_in_ms)
     }
 
-    fn keep_on_until_silence(&mut self, sensor_pin: &Input) -> Result<(), ClocklessRmtError> {
+    fn keep_on_until_silence(
+        &mut self,
+        motion_sensor: &impl MotionDetector,
+    ) -> Result<(), ClocklessRmtError> {
         self.light_on()?;
 
         self.delay.delay(GRACE_PERIOD);
@@ -97,19 +103,47 @@ impl<'a> Driver<'a> {
             // Grace period > light duration - grace period is already expired
             .unwrap_or(Duration::ZERO);
         loop {
-            if sensor_pin.is_high() {
+            if motion_sensor.motion_detected() {
                 info!("Motion reset!");
                 time_remaining = LIGHT_DURATION;
             }
 
             if time_remaining.le(&Duration::ZERO) {
                 info!("Clear");
-                self.light_off();
+                self.light_off()?;
                 return Ok(());
             }
 
             self.delay.delay(STEP);
             time_remaining = time_remaining.checked_sub(STEP).unwrap_or(Duration::ZERO);
+        }
+    }
+}
+
+// An event-driven approach would be much more adapted than the current pull method
+trait MotionDetector {
+    fn motion_detected(&self) -> bool;
+}
+
+struct PirMotionSensor<'p>(Input<'p>);
+
+impl<'p> MotionDetector for PirMotionSensor<'p> {
+    fn motion_detected(&self) -> bool {
+        self.0.is_high()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    struct MockMotionDetector {
+        returning: bool,
+    }
+
+    impl MotionDetector for MockMotionDetector {
+        fn motion_detected(&self) -> bool {
+            return self.returning;
         }
     }
 }
