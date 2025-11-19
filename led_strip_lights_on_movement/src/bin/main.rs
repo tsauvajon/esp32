d282a390174514de10203de4a7f4a29d8ad0bfb1
Thomas Sauvajon <thomas.sauvajon@proton.me>
2025-11-19 18:26:57 +0100

    Turn off at startup - also, extract a Driver struct

--- led_strip_lights_on_movement/src/bin/main.rs
+++ led_strip_lights_on_movement/src/bin/main.rs
@@ -6,6 +6,7 @@
     holding buffers for the duration of a data transfer."
 )]
 
+use blinksy_esp::ClocklessRmtError;
 use esp_backtrace as _;
 use esp_hal::clock::CpuClock;
 use esp_hal::delay::Delay;
@@ -13,7 +14,7 @@ use esp_hal::gpio::{Input, InputConfig, Pull};
 use esp_hal::main;
 use esp_hal::time::Duration;
 use log::info;
-use pir_motion_sensor::led_strip::build_led_controller;
+use pir_motion_sensor::led_strip::{RmtControl, build_led_controller};
 
 // This creates a default app-descriptor required by the esp-idf bootloader.
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
@@ -40,59 +41,80 @@ fn main() -> ! {
         InputConfig::default().with_pull(Pull::Down),
     );
 
-    let mut led_control = build_led_controller(peripherals.RMT, led_strip_data_pin);
+    const STEP: Duration = Duration::from_millis(STEP_MS);
 
-    let step = Duration::from_millis(STEP_MS);
-    let grace_period = Duration::from_millis(GRACE_PERIOD_MS);
     let delay = Delay::new();
-    led_control.set_brightness(0.05);
-    let elapsed_in_ms = blinksy_esp::time::elapsed().as_millis();
-    led_control.tick(elapsed_in_ms).unwrap();
+    let led_control = build_led_controller(peripherals.RMT, led_strip_data_pin);
+    let mut driver = Driver { led_control, delay };
+    driver.light_on().unwrap();
 
     info!("Delaying start - {STARTUP_DELAY_SEC} seconds!");
     delay.delay(Duration::from_secs(STARTUP_DELAY_SEC));
+
+    driver.light_off();
 
     // Avoid false positives
     let mut triggers = 0;
     loop {
         if sensor_pin.is_low() {
+            driver.light_off();
             triggers = 0;
-            delay.delay(step);
+            delay.delay(STEP);
             continue;
         }
 
         if triggers < 3 {
             info!("Trigger {triggers}");
             triggers += 1;
-            delay.delay(step);
+            delay.delay(STEP);
             continue;
         }
 
         triggers = 0;
         info!("Initial motion!");
-        led_control.set_brightness(0.05);
+
+        driver.keep_on_until_silence(&sensor_pin).unwrap();
+    }
+}
+
+struct Driver<'a> {
+    led_control: RmtControl<'a>,
+    delay: Delay,
+}
+
+impl<'a> Driver<'a> {
+    const STEP: Duration = Duration::from_millis(STEP_MS);
+    const GRACE_PERIOD: Duration = Duration::from_millis(GRACE_PERIOD_MS);
+
+    fn light_on(&mut self) -> Result<(), ClocklessRmtError> {
+        self.led_control.set_brightness(0.05);
         let elapsed_in_ms = blinksy_esp::time::elapsed().as_millis();
-        led_control.tick(elapsed_in_ms).unwrap();
+        self.led_control.tick(elapsed_in_ms)
+    }
+
+    fn light_off(&mut self) {
+        self.led_control.set_brightness(0.0);
+    }
+
+    fn keep_on_until_silence(&mut self, sensor_pin: &Input) -> Result<(), ClocklessRmtError> {
+        self.light_on()?;
 
         let mut ms_remaining = LIGHT_DURATION_MS;
-        delay.delay(grace_period);
+        self.delay.delay(Self::GRACE_PERIOD);
         ms_remaining -= GRACE_PERIOD_MS;
         loop {
-            delay.delay(step);
+            self.delay.delay(Self::STEP);
             ms_remaining -= STEP_MS;
 
             if ms_remaining <= 0 {
                 info!("Clear");
-                led_control.set_brightness(0.0);
-                let elapsed_in_ms = blinksy_esp::time::elapsed().as_millis();
-                led_control.tick(elapsed_in_ms).unwrap();
-                break;
+                self.light_off();
+                return Ok(());
             }
 
             if sensor_pin.is_high() {
                 info!("Motion reset!");
                 ms_remaining = LIGHT_DURATION_MS;
-                continue;
             }
         }
     }
